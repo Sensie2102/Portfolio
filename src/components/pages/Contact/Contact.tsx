f25b5816c04614720b408eb55c818869bcc8114e
Sensie21 <mafaaz.sabir@gmail.com>
2025-05-07 18:25:34 +0530

    Added Contact page complete

--- src/components/pages/Contact/Contact.tsx
+++ src/components/pages/Contact/Contact.tsx
@@ -1,8 +1,12 @@
+import ContactForm from '@/components/Forms/ContactForm'
 import React from 'react'
 
 const Contact = () => {
   return (
     <section id="contact" className='min-h-screen px-6 py-20 text-center flex flex-col items-center gap-12'>
+        <h1 className='text-heading-1 !text-[var(--accent)]'>Contact Me</h1>
+        <p className='text-body-muted leading-relaxed'></p>
+        <ContactForm />
     </section>
   )
 }
